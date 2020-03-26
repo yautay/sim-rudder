68c59763d3ca07ee2491ce74528072c46ec28909
Michal Pielaszkiewicz - PS42 <yautay@gmail.com>
2020-03-26 15:09:20 +0100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,55 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
+
 #include "Arduino.h"
+#include <Joystick.h>
+#include <Wire.h>
+#include <MCP3424.h>
+#include <EEPROM.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+#define JUMPERPIN 9
+#define GROUNDPIN 8
 
-void setup()
-{
-    // initialize LED digital pin as an output.
-    pinMode(LED_BUILTIN, OUTPUT);
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,0,0, false, false, false, true, true, true, false,
+                   false, false, false, false);
+
+void setup() {
+
+    /*
+ * Pin definitions
+ * Arduino no. 8 - ground pin for calibration jumper
+ * Arduino no. 9 - state pin for calibration jumper
+ * */
+
+pinMode(GROUNDPIN,OUTPUT);
+pinMode(JUMPERPIN,INPUT_PULLUP);
+digitalWrite(GROUNDPIN,LOW);
+
+Joystick.begin();
+
+/*
+ *
+ * */
+
+unsigned int leftBrakeMax;
+unsigned int rightBrakeMax;
+unsigned int yawMin;
+unsigned int yawMax;
+unsigned int temp;
+unsigned int leftBrakeMin;
+unsigned int rightBrakeMin;
+
+yawMin = ( EEPROM.read(0) | (EEPROM.read(1) << 8) );
+yawMax = ( EEPROM.read(2) | (EEPROM.read(3) << 8) );
+leftBrakeMin = ( EEPROM.read(4) | (EEPROM.read(5) << 8) );
+leftBrakeMax = ( EEPROM.read(6) | (EEPROM.read(7) << 8) );
+rightBrakeMin = ( EEPROM.read(8) | (EEPROM.read(9) << 8) );
+rightBrakeMax = ( EEPROM.read(10) | (EEPROM.read(11) << 8) );
+
+Joystick.setRzAxisRange(yawMin,yawMax);
+Joystick.setRxAxisRange(leftBrakeMin,leftBrakeMax);
+Joystick.setRyAxisRange(rightBrakeMin,rightBrakeMax);
+
 }
 
-void loop()
-{
-    // turn the LED on (HIGH is the voltage level)
-    digitalWrite(LED_BUILTIN, HIGH);
+void loop(){
 
-    // wait for a second
-    delay(1000);
-
-    // turn the LED off by making the voltage LOW
-    digitalWrite(LED_BUILTIN, LOW);
-
-    // wait for a second
-    delay(1000);
 }
